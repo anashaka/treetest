c3d6cc920e0c56dbf3f829fcc7449ef11220185e
Kirill Shtrykov <kirill@shtrykov.com>
2018-08-08 21:12:24 +0300

    Test commit #7

--- a/src/main/java/A.java
+++ a/src/main/java/A.java
@@ -9,5 +9,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println(new A().getGreeting());
 		System.out.println(new A().getGreeting());
+		System.out.println(new A().getGreeting());
     }
 }
